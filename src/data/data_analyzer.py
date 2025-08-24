9a69beacb6591394d46317baa7c0f49a364627a1
d541 <prince4albert@gmail.com>
2025-08-24 05:49:49 +0100

    added some functionality for the data analyzer script

--- src/data/data_analyzer.py
+++ src/data/data_analyzer.py
@@ -14,16 +14,35 @@ def analyze_dataset():
     determine the architecture requirements for the DataManager class
     """
 
-    data_path = Path('data/raw')
+    dir_path = Path('data/raw')
 
     # does the file exist in the given directory?
-    def discover_files(data_path, filename: str):
+    def discover_files(dir_path, filename: str):
         try:
-            file_path = Path(data_path) / filename # 'data_path' is the directory
+            file_path = Path(dir_path) / filename # 'dir_path' is the directory
             return file_path.exists() and file_path.is_file()
-        except (OSError, PermissionError):
+        except (OSError, PermissionError) as e:
+            print(f"There was an error finding the file in this directory: {e}")
             return False
 
     # it exists, how big is it?
-    def analyze_file_sizes():
-        pass
+    def analyze_file_sizes(file_path):
+        try:
+            return Path(file_path).stat().st_size
+        except (FileNotFoundError, OSError, PermissionError) as e:
+            print(f"There was an error analyzing the size of the file: {e}")
+            return None
+
+    # inspecting the data structure of the files
+    def inspect_data_structure(file_path):
+        try:
+            df = pd.read_csv(file_path)
+            return df.head(n = 5)
+        except (FileNotFoundError, OSError, PermissionError) as e:
+            print(f"There was an error analyzing the data structure: {e}")
+
+    # what matters to us in the file (target variable):
+    def identify_target_variable(file_path):
+        try:
+            df = pd.read_csv(file_path)
+
